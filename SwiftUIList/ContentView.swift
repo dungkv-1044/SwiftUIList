c5e993cd3e9325703bef0b3be6e420f534505b79
Dzung Khuat <dungkv95@gmail.com>
2019-07-08 09:45:38 +0700

    Initial Commit

---
+++ SwiftUIList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIList
+//
+//  Created by khuat.van.dung on 7/8/19.
+//  Copyright © 2019 khuat.van.dung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
